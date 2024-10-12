9fdea7061076d758e4570d7042135546587578a5
kris nur akbar <krisnurakbar@gmail.com>
2024-10-12 22:51:36 +0700

    sequilize fix

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const { Sequelize } = require('sequelize');
 // Use the DB_URL environment variable for the connection string
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
+  dialectModule: require('pg'),
   dialectOptions: {
     ssl: {
       require: true, // This is necessary for SSL connections
