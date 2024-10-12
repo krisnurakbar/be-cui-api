28e9322bd558602aa5efbb799d464f9ffc2026e5
kris nur akbar <krisnurakbar@gmail.com>
2024-10-12 23:23:29 +0700

    add debug db

--- config/database.js
+++ config/database.js
@@ -1,15 +1,14 @@
-const { Sequelize } = require('sequelize');
-
-// Use the DB_URL environment variable for the connection string
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   dialectModule: require('pg'),
   dialectOptions: {
     ssl: {
-      require: true, // This is necessary for SSL connections
-      rejectUnauthorized: false, // This helps with self-signed certificates
+      require: true,
+      rejectUnauthorized: false,
     },
   },
 });
 
-module.exports = sequelize;
+sequelize.authenticate()
+  .then(() => console.log('Database connection established successfully.'))
+  .catch(err => console.error('Unable to connect to the database:', err));
