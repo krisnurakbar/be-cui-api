b193f7de9df2dbd6da041e5fd05814a29967096f
kris nur akbar <krisnurakbar@gmail.com>
2024-10-12 23:31:12 +0700

    bug fix

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 const { Sequelize } = require('sequelize');
 
-// Use the DB_URL environment variable for the connection string
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   dialectModule: require('pg'),
@@ -12,7 +11,4 @@ const sequelize = new Sequelize(process.env.DB_URL, {
   },
 });
 
-sequelize.authenticate()
-  .then(() => console.log('Database connection established successfully.'))
-  .catch(err => console.error('Unable to connect to the database:', err));
-
+module.exports = sequelize;  // Ensure this line is correctly exporting the sequelize instance
